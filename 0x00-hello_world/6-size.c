ab6b044918a6839cc2e3ca19e1c1228e55ed69e6
justin <mbcframe@gmail.com>
2023-02-20 02:05:04 -0800

    merge

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,13 +7,12 @@
  * this progrsm prints 'the six=zes of everything we want it to print'
  * Return: 0
  */
-
 int main (void)
 {
 	printf("Size of char: %i byte(s)\n", sizeof(char));
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-	printf("Size of a floa: %i byte(s)\n", sizeof(float));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
 	return (0);
 }
