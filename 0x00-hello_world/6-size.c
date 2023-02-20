5094dce9bea383cfbcb47cc045af215a8df3e4d2
justin <mbcframe@gmail.com>
2023-02-20 01:16:20 -0800

    pool

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,12 @@
  * this progrsm prints 'the six=zes of everything we want it to print'
  * Return: 0
  */
+
 int main (void)
 {
 	printf("Size of char: %i byte(s)\n", sizeof(char));
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
-	printf("Size od=f a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("Size of a floa: %i byte(s)\n", sizeof(float));
 	return (0);
