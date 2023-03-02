b4e6c28ca6cb0d2cef03135b2c75e5cca0b3e42e
justin <mbcframe@gmail.com>
2023-03-01 22:25:44 -0800

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main -  entry point
+ * function generates random valid pssword
+ *
+ * Return: Always 0 (on success)
+ */
+
+int main(void)
+{
+	int i, j, sum;
+	char psswrd[100];
+
+	sum = 0;
+	srand(time(NULL));
+	for (i = 0; i < 100; i++)
+	{
+		psswrd[i] = rand() % 78;
+		sum = sum + (psswrd[i] + '0');
+		putchar(psswrd[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			j = 2772 - sum - '0';
+			sum = sum + j;
+			putchar(j + '0');
+			break;
+		}
+	}
+	return (0);
+}
